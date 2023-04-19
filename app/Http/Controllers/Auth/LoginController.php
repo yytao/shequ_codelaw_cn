307a5323208485908075c3dd38e26c00ac8a9bad
taoyy <tyyphp@163.com>
2023-04-19 14:08:30 +0800

    modify user model

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Request;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -41,8 +42,5 @@ class LoginController extends Controller
 
 
 
-    public function credentials(Request $request)
-    {
-        return array_merge($request->only($this->username(), 'password'),['is_verified' => 1]);
-    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
-
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
+
     protected $table = "users";
     protected $guarded = [];
 
